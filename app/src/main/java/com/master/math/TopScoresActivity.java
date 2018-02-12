08311c7cec022aaf01bf3a08e382a2091f77ed15
Miguel R. Laher <iamigslaher@gmail.com>
2018-02-12 16:16:00 +0800

    test

---
+++ app/src/main/java/com/master/math/TopScoresActivity.java
@@ -0,0 +1,29 @@
+package com.master.math;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.master.math.activity.MyAdapter;
+
+public class TopScoresActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_scores);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerTopScores);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
